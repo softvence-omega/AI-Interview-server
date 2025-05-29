eac99eec42b790bcef4635a6991acce043fdcf1d
Sakib-Dev7 <sakib.dev7@gmail.com>
2025-05-29 17:09:59 +0600

    something new added

--- src/modules/plan/plan.route.ts
+++ src/modules/plan/plan.route.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import PlanController from './plan.controller';
+import auth from '../../middlewares/auth';
+import { userRole } from '../../constents';
 
 const planRoutes = express.Router();
 
-planRoutes.post('/create-plan', PlanController.createPlan);
+planRoutes.post('/create-plan', auth([userRole.admin, userRole.user]), PlanController.createPlan);
 planRoutes.get('/all-plans', PlanController.getAllPlans);
 planRoutes.get('/single-plan/:id', PlanController.getPlanById);
-planRoutes.put('/update-plan/:id', PlanController.updatePlan);
-planRoutes.delete('/delete-plan/:id', PlanController.deletePlan);
+planRoutes.put('/update-plan/:id', auth([userRole.admin, userRole.user]), PlanController.updatePlan);
+planRoutes.delete('/delete-plan/:id', auth([userRole.admin, userRole.user]), PlanController.deletePlan);
 
 export default planRoutes;
